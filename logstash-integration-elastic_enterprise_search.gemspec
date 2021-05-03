60adff9b04728ac02789365f197ad502c6b38cd6
andsel <selva.andre@gmail.com>
2021-05-03 13:03:38 +0200

    Copied code from AppSearch output

--- logstash-integration-elastic_enterprise_search.gemspec
+++ logstash-integration-elastic_enterprise_search.gemspec
@@ -2,7 +2,7 @@ Gem::Specification.new do |s|
   s.name          = 'logstash-integration-elastic_enterprise_search'
   s.version         = '2.0.0'
   s.licenses        = ['Apache-2.0']
-  s.summary         = "Integration with Elasti Enterprise Search - output plugins"
+  s.summary         = "Integration with Elastic Enterprise Search - output plugins"
   s.description     = "This gem is a Logstash plugin required to be installed on top of the Logstash core pipeline "+
                       "using $LS_HOME/bin/logstash-plugin install gemname. This gem is not a stand-alone program."
   s.authors         = ["Elastic"]
@@ -16,11 +16,16 @@ Gem::Specification.new do |s|
   s.test_files = s.files.grep(%r{^(test|spec|features)/})
 
   # Special flag to let us know this is actually a logstash plugin
-  s.metadata = { "logstash_plugin" => "true", "logstash_group" => "output" }
+    # Special flag to let us know this is actually a logstash plugin
+  s.metadata = {
+      "logstash_plugin"     => "true",
+      "logstash_group"      => "integration",
+      "integration_plugins" => "logstash-output-elastic_app_search"
+  }
 
   # Gem dependencies
-  #
-  s.add_runtime_dependency "logstash-core-plugin-api", ">= 1.60", "<= 2.99"
+  s.add_runtime_dependency "logstash-core-plugin-api", "~> 2.0"
   s.add_runtime_dependency "logstash-codec-plain"
-  s.add_development_dependency 'logstash-devutils'
+  s.add_runtime_dependency "elastic-app-search", '~>7.8.0'
+  s.add_development_dependency "logstash-devutils"
 end
---
+++ spec/outputs/appsearch_spec.rb
@@ -0,0 +1,43 @@
+# encoding: utf-8
+require "logstash/devutils/rspec/spec_helper"
+require "logstash/outputs/elastic_app_search"
+require "logstash/codecs/plain"
+require "logstash/event"
+
+describe LogStash::Outputs::ElasticAppSearch do
+  let(:sample_event) { LogStash::Event.new }
+  let(:host) { "test-host" }
+  let(:api_key) { "my_key" }
+  let(:engine) { "test-engine" }
+  subject { described_class.new(config) }
+
+  describe "#register" do
+    before(:each) do
+      allow(subject).to receive(:check_connection!)
+    end
+    context "when host is configured" do
+      let(:config) { { "host" => host, "api_key" => api_key, "engine" => engine } }
+      it "does not raise an error" do
+        expect { subject.register }.to_not raise_error
+      end
+    end
+    context "when host and path is configured" do
+      let(:config) { { "host" => host, "api_key" => api_key, "engine" => engine, "path" => "/v1" } }
+      it "raises an error" do
+        expect { subject.register }.to raise_error(LogStash::ConfigurationError)
+      end
+    end
+    context "when host and url is configured" do
+      let(:config) { { "host" => host, "api_key" => api_key, "engine" => engine, "url" => "http://localhost:9300" } }
+      it "raises an error" do
+        expect { subject.register }.to raise_error(LogStash::ConfigurationError)
+      end
+    end
+    context "when neither host nor url is configured" do
+      let(:config) { { "api_key" => api_key, "engine" => engine } }
+      it "raises an error" do
+        expect { subject.register }.to raise_error(LogStash::ConfigurationError)
+      end
+    end
+  end
+end
